5bf3dbac1b469747ecd020087413b51ec1724bbc
Anto Christopher <anto@deepsource.io>
2024-01-11 12:03:23 +0530

    test: BOM autofix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function isEven(x) {
+﻿function isEven(x) {
   if ((x = 2 || x % 2 == 0)) {
     console.log(`${x} is even`);
   } else console.log(`${x} is odd`);
@@ -86,4 +86,5 @@ function isMatched(str) {
 
 function commentCheck(){
   console.log("no comment")
+  debugger
 }
